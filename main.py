31f72f2d96b19eb8c363f048fb4898c50ec62be7
Tosh Camille <tochecamille@gmail.com>
2016-05-11 11:14:59 +0200

    add random

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import urllib.request
 import urllib.parse
 import re
 import tweepy
+import random
 from time import sleep
 
 
@@ -11,7 +12,7 @@ def search(text):
     query_string = urllib.parse.urlencode({"search_query": text})
     html_content = urllib.request.urlopen("http://www.youtube.com/results?" + query_string)
     search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read().decode())
-    video = "http://www.youtube.com/watch?v=" + search_results[0]
+    video = "http://www.youtube.com/watch?v=" + search_results[random.randint(0, len(search_results) -1)]
 
     return video
 
